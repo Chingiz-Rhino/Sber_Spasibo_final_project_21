7f9f4731d16f2d3dcc2676d8c83e385b6e7e7149
Chingiz <chingiz-askarov@list.ru>
2023-11-02 23:54:42 +0300

    finish of Mobile app page tests

--- src/test/java/sber/spasibo/pages/MobileAppPagesPage.java
+++ src/test/java/sber/spasibo/pages/MobileAppPagesPage.java
@@ -2,39 +2,56 @@ package sber.spasibo.pages;
 
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.files.DownloadActions.click;
 
 public class MobileAppPagesPage {
     SelenideElement
             mobileAppPageTap = $("[href='/landings/app']"),
             headerOfChips = $(".appInfo__items"),
-            privilegeLevelTap = $(""),
-            transferOfBonusesTap = $(""),
-            balanceAndHistoryTap = $("");
+            chipsTaps = $(".app-buttons-wrapper");
 
 
+    //scroll
     public MobileAppPagesPage scrollPage() {
         $(".appInfo").scrollTo();
         return this;
     }
 
+    //open mobile page
     public MobileAppPagesPage openMobileAppPage() {
         mobileAppPageTap.click();
         return this;
     }
+
+    //balance and history
     public MobileAppPagesPage verifyBalanceAndHistoryPage() {
         headerOfChips.shouldHave(text("Управляйте бонусами"));
         return this;
     }
 
-    public MobileAppPagesPage verifyPrivilegeLevelPage() {
-        privilegeLevelTap.shouldHave(text(""));
+    // Privilege level
+    public MobileAppPagesPage tapOnPrivilegeLevelChips() {
+        chipsTaps.$(byText("Уровень привилегий")).shouldHave(text("Уровень привилегий")).click();
         return this;
     }
 
-    public MobileAppPagesPage verifyTransferOfBonusesPage() {
-        transferOfBonusesTap.shouldHave(text(""));
+    public MobileAppPagesPage verifyPrivilegeLevelPage() {
+        headerOfChips.shouldHave(text("Проверяйте уровень"));
         return this;
     }
+
+    //Transfer bonus
+    public MobileAppPagesPage tapOnTransferBonusesChips() {
+        chipsTaps.$(byText("Перевод бонусов")).shouldHave(text("Перевод бонусов")).click();
+        return this;
+    }
+
+    public MobileAppPagesPage verifyTransferBonusesPage(){
+        headerOfChips.shouldHave(text("Пользуйтесь выгодой"));
+        return this;
+    }
+
 }
--- src/test/java/sber/spasibo/tests/web/MobileAppPageTest.java
+++ src/test/java/sber/spasibo/tests/web/MobileAppPageTest.java
@@ -3,9 +3,10 @@ package sber.spasibo.tests.web;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.sleep;
 import static com.codeborne.selenide.logevents.SelenideLogger.step;
 
-public class MobileAppPageTest extends TestBase{
+public class MobileAppPageTest extends TestBase {
 
     @Test
     @DisplayName("Testing information about Balance and History on page MobileApp")
@@ -21,5 +22,42 @@ public class MobileAppPageTest extends TestBase{
             mobileAppPagesPage.verifyBalanceAndHistoryPage();
         });
     }
+
+    @Test
+    @DisplayName("Testing information about Privilege level on page MobileApp")
+    void informationAboutPrivilegeLevelTest() {
+        step("Open main page", () -> {
+            openWebSite();
+        });
+        step("Open Mobile app page", () -> {
+            mobileAppPagesPage.openMobileAppPage();
+            mobileAppPagesPage.scrollPage();
+        });
+        step("Tap on Privilege level chips", () -> {
+            mobileAppPagesPage.tapOnPrivilegeLevelChips();
+        });
+        step("Verify Privilege level information on page", () -> {
+            mobileAppPagesPage.verifyPrivilegeLevelPage();
+        });
+    }
+
+    @Test
+    @DisplayName("Testing information about Transfer bonus on page MobileApp")
+    void informationAboutTransferBonusTest() {
+        step("Open main page", () -> {
+            openWebSite();
+        });
+        step("Open Mobile app page", () -> {
+            mobileAppPagesPage.openMobileAppPage();
+            mobileAppPagesPage.scrollPage();
+        });
+        step("Tap on Transfer bonus chips", () -> {
+            mobileAppPagesPage.tapOnTransferBonusesChips();
+        });
+        step("Verify Transfer bonus information on page", () -> {
+            mobileAppPagesPage.verifyTransferBonusesPage();
+        });
+    }
+
 
 }
